d70dde191dcc81019b9d517035836c86f5441e4e
vmr <vmrbaez@gmail.com>
2024-08-16 23:41:44 -0600

    Se modificaron paginas que no se usaban

--- src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
+++ src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
@@ -1,129 +1,130 @@
-"use client"
+// "use client"
 
-import { placeOrder } from '@/actions';
-import { titleFont } from '@/config/fonts';
-import { useAddressStore, useCartStore } from '@/store';
-import { currencyFormat } from '@/utils';
-import clsx from 'clsx';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+
+// import { titleFont } from '@/config/fonts';
+// import { useAddressStore, useCartStore } from '@/store';
+// import { currencyFormat } from '@/utils';
+// import clsx from 'clsx';
+// import { useRouter } from 'next/navigation';
+// import React, { useEffect, useState } from 'react'
 
 export const PlaceOrder = () => {
 
-    const router = useRouter();
+//     const router = useRouter();
 
-    const [loaded, setLoaded] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [isPlacingOrder, setIsPlacingOrder] = useState(false);
+//     const [loaded, setLoaded] = useState(false);
+//     const [errorMessage, setErrorMessage] = useState('');
+//     const [isPlacingOrder, setIsPlacingOrder] = useState(false);
 
-    const address = useAddressStore(state => state.address);
+//     const address = useAddressStore(state => state.address);
     
-    const {itemInCart, total, subTotal} = useCartStore(state => state.getSumaryInformation());
+//     const {itemInCart, total, subTotal} = useCartStore(state => state.getSumaryInformation());
     
-    const cart = useCartStore(state => state.cart)
-    const clearCart = useCartStore(state => state.clearCart) 
+//     const cart = useCartStore(state => state.cart)
+//     const clearCart = useCartStore(state => state.clearCart) 
 
-    useEffect(() => {
-        setLoaded(true);
-    },[])
+//     useEffect(() => {
+//         setLoaded(true);
+//     },[])
     
-    // if(total === 0){
-    //     router.replace(`/cart`)
-    // }
+//     // if(total === 0){
+//     //     router.replace(`/cart`)
+//     // }
  
     
-    const onPlaceOrder = async() => {
-        setIsPlacingOrder(true);
+//     const onPlaceOrder = async() => {
+//         setIsPlacingOrder(true);
         
-        const productsToOrder = cart.map(producto => ({
-            productId: producto.id,
-            quantity: producto.quantity,
-            descuento: producto.descuento ?? 0,
+//         const productsToOrder = cart.map(producto => ({
+//             productId: producto.id,
+//             quantity: producto.quantity,
+//             descuento: producto.descuento ?? 0,
             
-        }))
+//         }))
         
-        //! server action
-        const resp = await placeOrder(productsToOrder, address);
-        if(!resp.ok){
+//         //! server action
+//         const resp = await placeOrder(productsToOrder, address);
+//         if(!resp.ok){
             
-            setIsPlacingOrder(false);
-            setErrorMessage(resp.message);
-            return;
+//             setIsPlacingOrder(false);
+//             setErrorMessage(resp.message);
+//             return;
             
-        }
-        // Todo bien
+//         }
+//         // Todo bien
 
-        router.replace(`/orders/` + resp.order!.id)
-        clearCart();
+//         router.replace(`/orders/` + resp.order!.id)
+//         clearCart();
 
         
-    }
+//     }
 
 
 
-    if(!loaded){
-        return <p>Cargando...</p>
-    }
+//     if(!loaded){
+//         return <p>Cargando...</p>
+//     }
 
 
 
     return (
-        <div className={`bg-white rounded-xl shadow-xl p-7`}>
+        // <div className={`bg-white rounded-xl shadow-xl p-7`}>
 
-            <h2 className={` text-4xl mb-2 font-bold`}>Dirección</h2>
-            <div className="mb-10">
-                <p className="text-xl">{address.firstName} {address.lastName}</p>
-                <p>{address.calle}</p>
-                <p>{address.localidad} </p>
-                <p>{address.estado}, {address.detalle}</p>
-                <p>{address.postalCode}</p>
-                <p>{address.phone}</p>
-                <p>{address.referencia}</p>
-            </div>
+        //     <h2 className={` text-4xl mb-2 font-bold`}>Dirección</h2>
+        //     <div className="mb-10">
+        //         <p className="text-xl">{address.firstName} {address.lastName}</p>
+        //         <p>{address.calle}</p>
+        //         <p>{address.localidad} </p>
+        //         <p>{address.estado}, {address.detalle}</p>
+        //         <p>{address.postalCode}</p>
+        //         <p>{address.phone}</p>
+        //         <p>{address.referencia}</p>
+        //     </div>
 
-            <div className="w-full h-0.5 bg-gray-200 mb-10" />
+        //     <div className="w-full h-0.5 bg-gray-200 mb-10" />
 
-            <h2 className="text-2xl mb-2">Resumen de orden</h2>
+        //     <h2 className="text-2xl mb-2">Resumen de orden</h2>
 
-            <div className="grid grid-cols-2">
-                <span>No.productos</span>
-                <span className="text-rigth">{itemInCart}</span>
+        //     <div className="grid grid-cols-2">
+        //         <span>No.productos</span>
+        //         <span className="text-rigth">{itemInCart}</span>
 
-               <span>Subtotal</span>
-                <span className="text-rigth">{currencyFormat(subTotal)}</span>
+        //        <span>Subtotal</span>
+        //         <span className="text-rigth">{currencyFormat(subTotal)}</span>
 
-                 {/* <span>Impuestos</span>
-                <span className="text-rigth">$100</span> */}
+        //          {/* <span>Impuestos</span>
+        //         <span className="text-rigth">$100</span> */}
 
-                <span className="mt-5 text-2xl">Total:</span>
-                <span className=" mt-5 text-2xl text-rigth">{currencyFormat(total)}</span>
-            </div>
+        //         <span className="mt-5 text-2xl">Total:</span>
+        //         <span className=" mt-5 text-2xl text-rigth">{currencyFormat(total)}</span>
+        //     </div>
 
-            <div className="mt-5 mb-2 w-full">
-                {/* <span className="mb-5">
-                    Al hacer click en &quot;Colocar Orden&quot;, aceptas nuestros <a href="#" className="underline"> terminos y condiciones</a>
-                </span> */}
+        //     <div className="mt-5 mb-2 w-full">
+        //         {/* <span className="mb-5">
+        //             Al hacer click en &quot;Colocar Orden&quot;, aceptas nuestros <a href="#" className="underline"> terminos y condiciones</a>
+        //         </span> */}
 
-            <p className='text-red-500'>{errorMessage}</p>
-                <span className="mb-5">
-                    Actualmente estamos trabajando en poder hacer envíos, por lo que solo está disponible la opción de recoger en tienda.
-                </span>
-                <button 
-                    className={
-                        clsx(
-                            "flex justify-center w-full mt-4" ,
-                            {
+        //     <p className='text-red-500'>{errorMessage}</p>
+        //         <span className="mb-5">
+        //             Actualmente estamos trabajando en poder hacer envíos, por lo que solo está disponible la opción de recoger en tienda.
+        //         </span>
+        //         <button 
+        //             className={
+        //                 clsx(
+        //                     "flex justify-center w-full mt-4" ,
+        //                     {
                              
-                            'btn-primary': !isPlacingOrder,
-                            'btn-disabled': isPlacingOrder
-                        })
-                    }
-                    onClick={onPlaceOrder}
-                >
-                    Recoger en tienda
-                </button>
-            </div>
+        //                     'btn-primary': !isPlacingOrder,
+        //                     'btn-disabled': isPlacingOrder
+        //                 })
+        //             }
+        //             onClick={onPlaceOrder}
+        //         >
+        //             Recoger en tienda
+        //         </button>
+        //     </div>
 
-        </div>
+        // </div>
+        <h1>place Order</h1>
     )
 }
--- src/app/admin/orders/[id]/page.tsx
+++ src/app/admin/orders/[id]/page.tsx
@@ -37,99 +37,100 @@ export default async function OrdersByIdPage({params}:Props) {
 
     
     return(
-        <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
-          <div className="flex flex-col w-[1000px]">
-            <Title title={`Orden #${id.split("-").at(-1)}`} />
+      <h1>order id</h1>
+        // <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+        //   <div className="flex flex-col w-[1000px]">
+        //     <Title title={`Orden #${id.split("-").at(-1)}`} />
     
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
-              {/* Carrito */}
-              <div className="flex flex-col mt-5">
-                <OrderStatus isPaid={order?.isPaid ?? false} />
+        //     <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+        //       {/* Carrito */}
+        //       <div className="flex flex-col mt-5">
+        //         <OrderStatus isPaid={order?.isPaid ?? false} />
     
-                {/* Items */}
-                {order!.OrderItem.map((item) => (
-                  <div
-                    key={item.product.slug }
-                    className="flex mb-5"
-                  >
-                    <Image
-                      src={`/products/${item.product.ProductImage[0].url}`}
-                      width={100}
-                      height={100}
-                      style={{
-                        width: "100px",
-                        height: "100px",
-                      }}
-                      alt={item.product.title}
-                      className="mr-5 rounded"
-                    />
+        //         {/* Items */}
+        //         {order!.OrderItem.map((item) => (
+        //           <div
+        //             key={item.product.slug }
+        //             className="flex mb-5"
+        //           >
+        //             <Image
+        //               src={`/products/${item.product.ProductImage[0].url}`}
+        //               width={100}
+        //               height={100}
+        //               style={{
+        //                 width: "100px",
+        //                 height: "100px",
+        //               }}
+        //               alt={item.product.title}
+        //               className="mr-5 rounded"
+        //             />
     
-                    <div>
-                      <p>{item.product.title}</p>
-                      <p>
-                        ${item.price} x {item.quantity}
-                      </p>
-                      <p className="font-bold">
-                        Subtotal: {currencyFormat(item.price * item.quantity)}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
+        //             <div>
+        //               <p>{item.product.title}</p>
+        //               <p>
+        //                 ${item.price} x {item.quantity}
+        //               </p>
+        //               <p className="font-bold">
+        //                 Subtotal: {currencyFormat(item.price * item.quantity)}
+        //               </p>
+        //             </div>
+        //           </div>
+        //         ))}
+        //       </div>
     
-              {/* Checkout - Resumen de orden */}
-              <div className="bg-white rounded-xl shadow-xl p-7">
-                <h2 className="text-2xl mb-2">Dirección de entrega</h2>
-                <div className="mb-10">
-                  <p className="text-xl">
-                    {address!.firstName} {address!.lastName}
-                  </p>
-                  <p>{address!.calle}</p>
-                  <p>{address!.detalle}</p>
-                  <p>{address!.localidad}</p>
-                  <p>
-                    {address!.estadoId}
-                  </p>
-                  <p>{address!.postalCode}</p>
-                  <p>{address!.phone}</p>
-                </div>
+        //       {/* Checkout - Resumen de orden */}
+        //       <div className="bg-white rounded-xl shadow-xl p-7">
+        //         <h2 className="text-2xl mb-2">Dirección de entrega</h2>
+        //         <div className="mb-10">
+        //           <p className="text-xl">
+        //             {address!.firstName} {address!.lastName}
+        //           </p>
+        //           <p>{address!.calle}</p>
+        //           <p>{address!.detalle}</p>
+        //           <p>{address!.localidad}</p>
+        //           <p>
+        //             {address!.estadoId}
+        //           </p>
+        //           <p>{address!.postalCode}</p>
+        //           <p>{address!.phone}</p>
+        //         </div>
     
-                {/* Divider */}
-                <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+        //         {/* Divider */}
+        //         <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
     
-                <h2 className="text-2xl mb-2">Resumen de orden</h2>
+        //         <h2 className="text-2xl mb-2">Resumen de orden</h2>
     
-                <div className="grid grid-cols-2">
-                  <span>No. Productos</span>
-                  <span className="text-right">
-                    {order?.itemsInOrder === 1
-                      ? "1 artículo"
-                      : `${order?.itemsInOrder} artículos`}
-                  </span>
+        //         <div className="grid grid-cols-2">
+        //           <span>No. Productos</span>
+        //           <span className="text-right">
+        //             {order?.itemsInOrder === 1
+        //               ? "1 artículo"
+        //               : `${order?.itemsInOrder} artículos`}
+        //           </span>
     
-                  <span>Subtotal</span>
-                  <span className="text-right">
-                    {currencyFormat(order!.subTotal)}
-                  </span>
+        //           <span>Subtotal</span>
+        //           <span className="text-right">
+        //             {currencyFormat(order!.subTotal)}
+        //           </span>
     
 
     
-                  <span className="mt-5 text-2xl">Total:</span>
-                  <span className="mt-5 text-2xl text-right font-bold">
-                    {currencyFormat(order!.total)}
-                  </span>
-                </div>
+        //           <span className="mt-5 text-2xl">Total:</span>
+        //           <span className="mt-5 text-2xl text-right font-bold">
+        //             {currencyFormat(order!.total)}
+        //           </span>
+        //         </div>
     
-                <div className="mt-5 mb-2 w-full">
-                  {order?.isPaid ? (
-                    <OrderStatus isPaid={order?.isPaid ?? false} />
-                  ) : (
-                    <TiendaButton  orderId={order!.id} order={order!} />
-                  )}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        //         <div className="mt-5 mb-2 w-full">
+        //           {order?.isPaid ? (
+        //             <OrderStatus isPaid={order?.isPaid ?? false} />
+        //           ) : (
+        //             <TiendaButton  orderId={order!.id} order={order!} />
+        //           )}
+        //         </div>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
       );
 }
